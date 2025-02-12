63aae11fc58843220308d75db159f4dc8b1ecb21
ken sofware engineer <mainakenken188@gmail.com>
2025-02-12 14:23:04 +0300

    bbb

---
+++ index.js
@@ -0,0 +1,39 @@
+//track count
+//count occurences
+//
+
+//function solution(A) {
+    
+    //const departmentMap = new Map();
+    
+   // let maxPatients = 0;
+    
+   
+   // for (const department of A) {
+        
+       // const currentCount = (departmentMap.get(department) || 0) + 1;
+        //departmentMap.set(department, currentCount);
+        
+        
+        //maxPatients = Math.max(maxPatients, currentCount);
+    //}
+    
+    //return maxPatients;
+//}
+
+//const test1 = ["Cardiology", "Orthopaedics", "Neurology", "Cardiology", "Orthopaedics", "Cardiology"];
+//const test2 = ["Oncology", "Gynaecology", "Orthopaedics", "Oncology", "Gynaecology", "Orthopaedics"];
+//const test3 = ["Neurology", "Cardiology", "Oncology"];
+
+//console.log(solution(test2)); 
+
+function solution(A){
+    const maxPatients=O
+}
+
+for (const Patients in A){
+    
+}
+    
+    
+    return maxPatients;
