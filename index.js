a64f8f16b035a3289624956e8ac9f09bdfe401e8
ken sofware engineer <mainakenken188@gmail.com>
2025-02-13 04:22:51 +0300

    mmm codilities

--- index.js
+++ index.js
@@ -27,13 +27,51 @@
 
 //console.log(solution(test2)); 
 
-function solution(A){
-    const maxPatients=O
-}
+//function solution(A){
+    //const maxPatients=O
+//}
 
-for (const Patients in A){
+//for (const Patients in A){
     
-}
+//}
     
     
-    return maxPatients;
+    //return maxPatients;
+
+
+    //const students= ["a", "b", "b", "c", "a"]
+
+   // let count = {};
+
+    //students.forEach(item =>{
+       // if(count[item]){
+         //  count[item]++
+       // }else{
+           // count[item]=1
+       // }
+    //})
+
+   // console .log(count)
+
+    // Define an array of numbers
+//const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Function to check if a number is even
+//function isEven(value) {
+    //return value % 2 === 0;
+//}
+
+// Function to check if a number is odd
+//function isOdd(value) {
+    //return value % 2 !== 0;
+//}
+
+// Filter the array to get even numbers
+//const evenNumbers = numbers.filter(isEven);
+
+// Filter the array to get odd numbers
+//const oddNumbers = numbers.filter(isOdd);
+
+// Log the results
+//console.log("Even numbers:", evenNumbers);
+//console.log("Odd numbers:", oddNumbers);
